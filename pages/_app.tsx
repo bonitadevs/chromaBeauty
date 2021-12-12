6fd8c50da38c31af46d3486badb221bc68f4c296
Dani <ddw29@me.com>
2021-12-11 22:41:24 -0500

    Uninstalled Material Ui and Dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,12 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+  <div className="App">
+</div>
+<Component {...pageProps} />
+</>
+
 }
 
 export default MyApp
